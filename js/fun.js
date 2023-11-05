1577ee25ab85e2c15c8d089678c49e5ff4b1c188
Oleksandr Kovaliuk <133936726+oleksandrkovaliuk@users.noreply.github.com>
2023-11-05 14:36:52 -0700

    Revert "Remove dafault style for autofill"

--- js/fun.js
+++ js/fun.js
@@ -1,11 +1,9 @@
 const cursor = document.querySelector(".follow-cursor");
 
-cursor.addEventListener("mousemove", (event) => {
+cursor.addEventListener("mousemove", () => {
   const x = Math.random() * 100;
-  console.log(x);
   const y = Math.random() * 100;
-  console.log(y);
-  cursor.style.left = (x + "px");
+  cursor.style.left = x + "px";
   cursor.style.top = y + "px";
   cursor.style.zIndex = "5";
 });
