172e0e2dc59260a56838503dc9b2f3625ba868a5
Alex <sasha1235qaz@ukr.net>
2023-11-25 14:40:13 -0700

    all pop up menu

--- js/catalogMain.js
+++ js/catalogMain.js
@@ -44,3 +44,20 @@ bindEventOnBuyBtn(document.querySelectorAll(".buyBtn"));
 
 export { filterCards, bindEventOnBuyBtn , checkItemInCart};
 checkItemInCart();
+
+
+
+const burgerOpenMenu = document.querySelector('.burger');
+const closeBtnMenu = document.querySelector('.close-btn');
+const blurManuBg = document.querySelector('.blur-background-black');
+  const mobileMenu = document.querySelector('.mobile-menu-wrap');
+  burgerOpenMenu.addEventListener('click' , () => {
+mobileMenu.style.transform = "translateX(0)";
+blurManuBg.style.visibility = "visible";
+blurManuBg.style.transform = "translateX(0)";
+})
+closeBtnMenu.addEventListener('click' , () => {
+  mobileMenu.style.transform = "translateX(100%)"
+blurManuBg.style.transform = "translateX(100%)";
+})
+
